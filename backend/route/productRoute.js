de57c6cfcb2fcbac194003ee8afa81b33baf69f3
Lisa ^^ <islahola>
2023-03-03 06:13:40 +0700

    first commit

---
+++ backend/route/productRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+ 
+import { 
+    getAllProducts,
+    createProduct,
+    getProductById,
+    updateProduct,
+    deleteProduct
+} from "../controller/product.js";
+ 
+const router = express.Router();
+ 
+router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.patch('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
+export default router
+ 
